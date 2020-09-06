6d6bce554ce96f45cb64747a1d150a5ccb5a18b0
khushiverma <44372851+Khushi-Verma@users.noreply.github.com>
2020-09-06 14:37:33 +0530

    print max elements's index input by user

---
+++ UserInputMax.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MaximumNumber {
+    public static int findIndexOfMax(int[] numbers) {
+        int index = 0;
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > numbers[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] numbers = new int[n];
+        for(int i=0;i<numbers.length;i++){
+            numbers[i]=scanner.nextInt();
+        }
+        System.out.println(MaximumNumber.findIndexOfMax(numbers));
+    }
+}
